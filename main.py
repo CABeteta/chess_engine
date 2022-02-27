4bd58090a48b59f652facdc3c1fa15f1936af753
Carlos Abellan Beteta <carlos.abellan.beteta@cern.ch>
2022-02-27 19:41:59 +0100

    Now with keyboard inputs

--- main.py
+++ main.py
@@ -19,42 +19,48 @@ def main():
             elif e.type == p.MOUSEBUTTONDOWN:
                 click_location = p.mouse.get_pos() # (x, y) of the mouse
                 if(e.button == p.BUTTON_LEFT):
-                    if InitialSquare == ".":
+                    if InitialSquare == ".": #Selecting origin
                         InitialSquare = myBoardWindow.getSquareFromXY(click_location)
                         if (myBoard.turn == myBoardWindow.pieceColor(InitialSquare) and myBoardWindow.squareIsEmpty(InitialSquare)==False) :
                             myBoardWindow.selectSquare(InitialSquare)
-                            #print("InitialSquare: "+InitialSquare)
-                        else:
+                        else: #Invalid first selection, ignoring
                             InitialSquare="."
-                            #print("Tried to select a wrong square")
-                    else:
+                    else: #Selecting destination
                         FinalSquare = myBoardWindow.getSquareFromXY(click_location)
                         currentMove = InitialSquare.lower()+FinalSquare.lower()
-                        if InitialSquare == FinalSquare :
-                            #print("Piece dropped.")
-                            InitialSquare="." # So the loop starts again
+                        if InitialSquare == FinalSquare: #Unselecting
+                            InitialSquare="." # So next time we select origin again
                             FinalSquare="."
                             myBoardWindow.selectSquare(InitialSquare) #Unselects
-                        elif c.Move.from_uci(currentMove) in myBoard.legal_moves:
+                        elif c.Move.from_uci(currentMove) in myBoard.legal_moves: #Legal move
                             myBoard.push_uci(currentMove)
                             myBoardWindow.movePiece(InitialSquare, FinalSquare)
                             myBoardWindow.updateBoard()
-                            #print("FinalSquare: "+FinalSquare)
-                            InitialSquare="." # So the loop starts again
+                            InitialSquare="." # So next time we select origin again
                             FinalSquare="."
                             if myBoard.is_checkmate() :
                                 print("Checkmate!")
                             elif myBoard.is_stalemate() :
                                 print("Stalemate!")
-                        else:
-                            #print("FinalSquare: "+FinalSquare)
-                            #print("Move "+InitialSquare+FinalSquare+" is ilegal")
+                        else: # The move was ilegal
                             FinalSquare="."
                 elif myBoard.move_stack!=[]: #If move stack isn't empty
                     myBoard.pop()
                     lastDestination=c.square_name(myBoard.peek().to_square).lower()
                     myBoardWindow.updatePosition(myBoard.piece_map(), lastDestination)
                     myBoardWindow.updateBoard()
+            elif e.type == p.KEYDOWN:
+                if e.key  == p.K_u:
+                    print("UNDO")
+                elif e.key == p.K_r:
+                    print("ROOK")
+                elif e.key == p.K_q:
+                    print("QUEEN")
+                elif e.key == p.K_b:
+                    print("BISHOP")
+                elif e.key == p.K_k:
+                    print("KNIGHT")
+
 
 
 if __name__ == "__main__":
